9a213ecdb32bd39c9a36d8de427dea22eaec6b19
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2023-08-09 13:21:34 +0200

    Update LSS-Gebäude Löschen.user.js

--- LSS-Gebäude Löschen.user.js
+++ LSS-Gebäude Löschen.user.js
@@ -1,69 +1,15 @@
 // ==UserScript==
 // @name         LSS-Gebäude löschen
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Fügt einen Button hinzu, um alle Gebäude zu löschen
 // @author       MissSobol
 // @match        https://www.leitstellenspiel.de/profile/*
 // @grant        none
 // ==/UserScript==
 
-(async function() {
-    'use strict';
-
-    // Erstelle einen Button
-    const deleteButton = document.createElement('button');
-    deleteButton.innerText = 'Gebäude abreißen';
-    deleteButton.style.margin = '5px';
-    deleteButton.onclick = startDeleteProcess;
-    document.querySelector('.container-fluid').prepend(deleteButton);
-
-    async function startDeleteProcess() {
-        const confirmFirst = confirm('Bist du sicher, dass du alle Gebäude abreißen möchtest? Das kann nicht rückgängig gemacht werden!');
-        if (confirmFirst) {
-            const userConfirmation = prompt('Du bist dabei alle Gebäude abzureißen. Um das zu bestätigen gib bitte NEUSTART ein');
-            if (userConfirmation === 'NEUSTART') {
-                const buildings = await getBuildingIds();
-                for (const buildingId of buildings) {
-                    await deleteAndCloseBuilding(buildingId);
-                    await wait(500); // Warte 500ms vor dem Öffnen des nächsten Gebäudes
-                }
-                alert('Alle Gebäude wurden abgerissen!');
-            } else {
-                alert('Bestätigung falsch. Vorgang abgebrochen!');
-            }
-        } else {
-            alert('Vorgang abgebrochen!');
-        }
-    }
-
-    // Funktion, um die Gebäude-IDs zu erhalten
-    async function getBuildingIds() {
-        const response = await fetch('https://www.leitstellenspiel.de/api/buildings');
-        const data = await response.json();
-        return data.map(building => building.id);
-    }
-
-    // Funktion, um ein Gebäude zu löschen, Tab zu schließen
-    async function deleteAndCloseBuilding(buildingId) {
-        const url = `https://www.leitstellenspiel.de/buildings/${buildingId}`;
-        const newTab = window.open(url, '_blank');
-        await wait(1000); // Warte 1 Sekunde, bis die Seite geladen ist
-        const deleteButton = newTab.document.querySelector('#delete_no_refund');
-        if (deleteButton) {
-            deleteButton.click(); // Klick auf den "Löschen"-Button
-            await wait(1000); // Warte 1 Sekunde, bis die nachfolgende Abfrage erscheint
-            // Simuliere die Bestätigung der Abfrage
-            newTab.confirm = () => true;
-        }
-        // Schließe den Tab nach dem Löschen
-        await wait(500); // Warte 500ms, bevor den Tab zu schließen
-        newTab.close();
-    }
-
-    // Funktion, um eine Wartezeit zu ermöglichen
-    function wait(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-})();
+//Lieber Scriptnutzer,
+//schweren Herzens habe ich heute alle meine Scripte deaktiviert. Der Betreiber hat mir meinen Forenaccount gesperrt, will mir aber weder verraten warum, noch ist man gewillt in Diskurs mit mir zu treten. Da ich ohne einen Forenaccount nicht das Level an Support geben kann, welches ich für mich selber als erforderlich betrachte, habe ich mit sofortiger Wirkung alle meine Scripte deaktiviert.
+//Ich bedanke mich für 5 Jahre Zusammenarbeit im Forum. Danke an alle, die meine Scripte genutzt haben, Daumen dagelassen haben und Kommentare geschrieben haben.
+//Liebe Grüße und noch viel Spaß
+//Sobol
